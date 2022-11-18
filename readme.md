8d3abab18c33562021246125c5c5f64e4bc9855e
kekeimiku <kekelanact@gmail.com>
2022-11-19 06:37:20 +0800

    clean

--- readme.md
+++ readme.md
@@ -1,11 +1,20 @@
-RFC 6238: TOTP: Time-Based One-Time Password Algorithm
+# totp-lite
 
-support sha256/sha512
-
+totp-lite 是rust的一个轻量 [TOTP](https://en.wikipedia.org/wiki/Time-based_one-time_password) 实现，支持 sha256/sha512
 ```rust
-let totp = TOTP::new(Secret::new(), "MY-PC", "user@localhost");
+let secret = "AZKXE3W57Q53ESFUWY4TKEAXIGLC7STL".as_bytes();
+// let secret: Vec<u8> = repeat_with(|| fastrand::u8(..)).take(20).collect();
+
+let totp = TOTP::with_default(secret, "MY-PC", "user@localhost");
 let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-totp.generate(timestamp);
-println!("{}", totp.get_url());
-println!("{}", totp.verify("you code", timestamp));
+let url = totp.get_url();
+println!("{}", url);
+println!("{}", totp.verify("input you code", timestamp));
+
+// let qrcode = fast_qr::QRBuilder::new(url).build().unwrap();
+// let _svg = SvgBuilder::default().shape(Shape::RoundedSquare).to_file(&qrcode, "out.svg");
 ```
+旨在与其它crates配合使用，而不需要强制下载几十个根本用不上的crates
+
+## 极简主义
+最小设计，牢记[UNIX 精神](https://en.wikipedia.org/wiki/Unix_philosophy#Do_One_Thing_and_Do_It_Well)，做一件事，并将其做好。
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,4 @@
-use core::{fmt::Display, iter::repeat_with};
-
 use hmac::{Hmac, Mac};
-
-#[derive(Debug)]
-pub struct Secret(pub Vec<u8>);
-
-#[allow(clippy::new_without_default)]
-impl Secret {
-    pub fn new() -> Self {
-        Self(repeat_with(|| fastrand::u8(..)).take(20).collect())
-    }
-
-    pub fn from<S: AsRef<str>>(secret: S) -> Self {
-        Self(base32::decode(base32::Alphabet::RFC4648 { padding: false }, secret.as_ref()).unwrap())
-    }
-
-    pub fn get_base32(&self) -> String {
-        base32::encode(base32::Alphabet::RFC4648 { padding: false }, &self.0)
-    }
-}
 
 #[derive(Debug, PartialEq)]
 pub enum Algorithm {
@@ -26,8 +6,8 @@ pub enum Algorithm {
     SHA512,
 }
 
-impl Display for Algorithm {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl core::fmt::Display for Algorithm {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
             Algorithm::SHA256 => f.write_str("SHA256"),
             Algorithm::SHA512 => f.write_str("SHA512"),
@@ -36,57 +16,63 @@ impl Display for Algorithm {
 }
 
 impl Algorithm {
-    fn hash<D>(mut digest: D, data: &[u8]) -> Vec<u8>
-    where
-        D: Mac,
-    {
+    fn hash<T: Mac>(mut digest: T, data: &[u8]) -> Vec<u8> {
         digest.update(data);
         digest.finalize().into_bytes().to_vec()
     }
 
-    fn sign(&self, key: &Secret, data: &[u8]) -> Vec<u8> {
+    fn sign<S: AsRef<[u8]>, D: AsRef<[u8]>>(&self, secret: S, data: D) -> Vec<u8> {
         match self {
-            Algorithm::SHA256 => {
-                Algorithm::hash(<Hmac<sha2::Sha256> as Mac>::new_from_slice(&key.0).unwrap(), data)
-            }
-            Algorithm::SHA512 => {
-                Algorithm::hash(<Hmac<sha2::Sha512> as Mac>::new_from_slice(&key.0).unwrap(), data)
-            }
+            Algorithm::SHA256 => Algorithm::hash(
+                <Hmac<sha2::Sha256> as Mac>::new_from_slice(secret.as_ref()).unwrap(),
+                data.as_ref(),
+            ),
+            Algorithm::SHA512 => Algorithm::hash(
+                <Hmac<sha2::Sha512> as Mac>::new_from_slice(secret.as_ref()).unwrap(),
+                data.as_ref(),
+            ),
         }
     }
 }
 
 #[derive(Debug)]
-pub struct TOTP<S> {
-    pub alg: Algorithm,
-    pub digits: usize,
-    pub skew: u8,
-    pub step: u64,
-    pub secret: Secret,
-    pub issuer: S,
-    pub account: S,
+pub struct TOTP<S, I, A> {
+    alg: Algorithm,
+    digits: usize,
+    skew: u8,
+    step: u64,
+    secret: S,
+    issuer: I,
+    account: A,
 }
 
-impl<S: AsRef<str>> TOTP<S> {
-    pub fn new(secret: Secret, issuer: S, account: S) -> TOTP<S> {
-        let secret = secret;
+impl<S: AsRef<[u8]>, I: AsRef<str>, A: AsRef<str>> TOTP<S, I, A> {
+    pub fn with_default(secret: S, issuer: I, account: A) -> Self {
         assert!(
-            !(secret.0.len() < 16 || issuer.as_ref().contains(':') || account.as_ref().contains(':')),
+            !(secret.as_ref().len() < 16 || issuer.as_ref().contains(':') || account.as_ref().contains(':')),
             "wrong format"
         );
         let alg = Algorithm::SHA256;
         let digits = 6;
         let skew = 1;
         let step = 30;
-        TOTP { alg, digits, skew, step, secret, issuer, account }
+        Self { alg, digits, skew, step, secret, issuer, account }
     }
 
-    fn sign(&self, time: u64) -> Vec<u8> {
-        self.alg.sign(&self.secret, &(time / self.step).to_be_bytes())
+    pub fn new(alg: Algorithm, digits: usize, skew: u8, step: u64, secret: S, issuer: I, account: A) -> Self {
+        assert!(
+            !(secret.as_ref().len() < 16
+                || (6..=8).contains(&digits)
+                || issuer.as_ref().contains(':')
+                || account.as_ref().contains(':')),
+            "wrong format"
+        );
+
+        Self { alg, digits, skew, step, secret, issuer, account }
     }
 
     pub fn generate(&self, time: u64) -> String {
-        let result: &[u8] = &self.sign(time);
+        let result = self.alg.sign(&self.secret, (time / self.step).to_be_bytes());
         let offset = (result.last().unwrap() & 15) as usize;
         let result = u32::from_be_bytes(result[offset..offset + 4].try_into().unwrap()) & 0x7fff_ffff;
         format!("{1:00$}", self.digits, result % 10_u32.pow(self.digits as u32))
@@ -94,15 +80,15 @@ impl<S: AsRef<str>> TOTP<S> {
 
     pub fn get_url(&self) -> String {
         let label = format!("{0}:{1}?issuer={0}&", self.issuer.as_ref(), self.account.as_ref());
-        let secret = self.secret.get_base32();
+        let secret = base32::encode(base32::Alphabet::RFC4648 { padding: false }, self.secret.as_ref());
         format!("otpauth://totp/{label}secret={secret}&digits={}&algorithm={}", self.digits, self.alg)
     }
 
-    pub fn verify(&self, token: &str, time: u64) -> bool {
+    pub fn verify<T: AsRef<str>>(&self, token: T, time: u64) -> bool {
         let basestep = time / self.step - (self.skew as u64);
         for i in 0..self.skew * 2 + 1 {
             let step_time = (basestep + (i as u64)) * self.step;
-            if self.generate(step_time) == token {
+            if self.generate(step_time) == token.as_ref() {
                 return true;
             }
         }
